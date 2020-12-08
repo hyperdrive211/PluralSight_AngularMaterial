66d21a972d47256291cfdd1cee261936bc1711c7
Johnny <jmccauley117@gmail.com>
2020-12-08 21:14:35 +0000

    added needed components for contact manager

--- my-app/src/app/contactmanager/contactmanager.module.ts
+++ my-app/src/app/contactmanager/contactmanager.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms'; 
+import {MaterialModule} from '../shared/material-module/material-module'; 
+import {FlexLayoutModule} from '@angular/flex-layout'; 
 import { ContactmanagerAppComponent } from './contactmanager-app/contactmanager-app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 
 
 @NgModule({
-  declarations: [ContactmanagerAppComponent],
+  declarations: [ContactmanagerAppComponent, ToolbarComponent, MainContentComponent, SidenavComponent],
   imports: [
     CommonModule
   ]
