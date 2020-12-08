70da416294e04df895ee6fda12c9fff3f15142b2
Johnny <jmccauley117@gmail.com>
2020-12-08 20:46:11 +0000

    generate components
    begun to set up the contact app

---
+++ my-app/src/app/contactmanager/contactmanager.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactmanagerAppComponent } from './contactmanager-app/contactmanager-app.component';
+
+
+
+@NgModule({
+  declarations: [ContactmanagerAppComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContactmanagerModule { }
--- my-app/src/app/demo/demo.module.ts
+++ my-app/src/app/demo/demo.module.ts
@@ -5,15 +5,18 @@ import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { MaterialModule } from '../shared/material-module/material-module';
 import { FormsModule } from '@angular/forms';
+import { FlexboxComponent } from './flexbox/flexbox.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
-  declarations: [ButtonsComponent],
+  declarations: [ButtonsComponent, FlexboxComponent],
   imports: [
     CommonModule,
     DemoRoutingModule, 
     MaterialModule, 
-    FormsModule
+    FormsModule, 
+    FlexLayoutModule
   ]
 })
 export class DemoModule { }
