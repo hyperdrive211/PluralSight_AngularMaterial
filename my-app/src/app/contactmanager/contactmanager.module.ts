cecb68f85ee67187217e23b63fdccb6508755798
Johnny <jmccauley117@gmail.com>
2020-12-09 14:00:36 +0000

    mark up inluded components

--- my-app/src/app/contactmanager/contactmanager.module.ts
+++ my-app/src/app/contactmanager/contactmanager.module.ts
@@ -7,13 +7,27 @@ import { ContactmanagerAppComponent } from './contactmanager-app/contactmanager-
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import {Routes, RouterModule} from '@angular/router'; 
 
+const routes: Routes = [
+  {path: '', component: ContactmanagerAppComponent, 
+  children: [
+    {path: '', component: MainContentComponent }, 
 
+  ]
+}, {
+  path: '**', redirectTo:'contactmanager'
+}
+]
 
 @NgModule({
   declarations: [ContactmanagerAppComponent, ToolbarComponent, MainContentComponent, SidenavComponent],
   imports: [
-    CommonModule
+    CommonModule, 
+    MaterialModule,
+    FormsModule, 
+    FlexLayoutModule, 
+    RouterModule.forRoot(routes)
   ]
 })
 export class ContactmanagerModule { }
